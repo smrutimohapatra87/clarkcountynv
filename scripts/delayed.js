b6fa31cad16410b3d82f661c8b2502356142a148
meejain <37831850+meejain@users.noreply.github.com>
2024-11-14 03:14:00 +0530

    Issue3 widget (#13)
    * Privisioning the accessibility widget
    
    * Adding the logic for screen resize for widget. In mobile - display = off
    
    * Removing the extra code & authenticating with user id
    
    * Fixing the lint issues
    
    ---------
    
    Co-authored-by: Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,27 @@
 // add delayed functionality here
+import { loadScript, sampleRUM } from './aem.js';
+
+// Core Web Vitals RUM collection
+sampleRUM('cwv');
+
+const isDesktop = window.matchMedia('(min-width: 900px)');
+
+async function loadWidget() {
+  await loadScript('/widgets/accessibility/accessibility.js');
+}
+
+if (isDesktop.matches) {
+  loadWidget();
+}
+
+function resizeAction() {
+  if (!isDesktop.matches) {
+    const div = document.querySelector('.userway_buttons_wrapper .uai.userway_dark');
+    div.style.setProperty('display', 'none');
+  } else {
+    const div = document.querySelector('.userway_buttons_wrapper .uai.userway_dark');
+    div.style.setProperty('display', 'block');
+  }
+}
+
+window.addEventListener('resize', resizeAction);
---
+++ widgets/accessibility/accessibility.js
@@ -0,0 +1,10 @@
+function widget() {
+  const s = document.createElement('script');
+  s.setAttribute('data-position', 5);
+  s.setAttribute('data-color', '#0094C3');
+  s.setAttribute('data-account', 'sudb1U5E4V');
+  s.setAttribute('src', 'https://cdn.userway.org/widget.js');
+  (document.body || document.head).appendChild(s);
+}
+
+widget();
