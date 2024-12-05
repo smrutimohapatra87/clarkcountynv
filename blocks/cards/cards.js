9becb3edb658a4fed74ac7be32a9e8deff22ac1c
meejain <37831850+meejain@users.noreply.github.com>
2024-12-05 19:57:29 +0530

    Updating a variant clickable for cards (#59)
    * Updating a variant clickable for cards
    
    * Leveraging the DOM Elements
    
    ---------
    
    Co-authored-by: Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,39 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import {
+  ul, li, a,
+} from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  const ul = document.createElement('ul');
+  const $ul = ul();
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+    if (block.classList.contains('clickable')) {
+      const $li = li();
+      const aEle = a();
+      aEle.append($li);
+      while (row.firstElementChild) $li.append(row.firstElementChild);
+      [...$li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+        else {
+          div.className = 'cards-card-body';
+          if (div.querySelector('a')) {
+            aEle.href = div.querySelector('a').href;
+            div.querySelector('a').remove();
+          }
+        }
+      });
+      $ul.append(aEle);
+    } else {
+      const $li = li();
+      while (row.firstElementChild) $li.append(row.firstElementChild);
+      [...$li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+        else div.className = 'cards-card-body';
+      });
+      $ul.append($li);
+    }
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  $ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
-  block.append(ul);
+  block.append($ul);
 }
