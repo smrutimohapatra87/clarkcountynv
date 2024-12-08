8a6ca790a7f81cbdd8cde7d7399837d46b9298e7
meejain <37831850+meejain@users.noreply.github.com>
2024-12-09 03:59:17 +0530

    Calendar Event Instrumentation (#69)
    * Testing the bug
    
    * Testing the bug
    
    * Testing the bug
    
    * Adding the logic of getting data from events and then parse its metadata into events sheet
    
    ---------
    
    Co-authored-by: Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>

--- templates/event/event.js
+++ templates/event/event.js
@@ -5,15 +5,15 @@ import { getMetadata } from '../../scripts/aem.js';
 export default async function decorate(doc) {
   doc.querySelector('header').remove();
   doc.querySelector('footer').remove();
-  doc.body.classList.add('calendar-event');
+  doc.body.classList.add('event');
 
   // Change the background color of the hero date & time based on the division-color metadata
   const divisionColor = getMetadata('division-color');
   if (divisionColor) {
-    doc.querySelectorAll('body.calendar-event main .section.hero .hero-wrapper .block.hero div div').forEach((element) => {
+    doc.querySelectorAll('body.event main .section.hero .hero-wrapper .block.hero div div').forEach((element) => {
       element.style.backgroundColor = divisionColor;
     });
-    doc.querySelectorAll('body.calendar-event main .section.event-footer .default-content-wrapper p > a').forEach((element) => {
+    doc.querySelectorAll('body.event main .section.event-footer .default-content-wrapper p > a').forEach((element) => {
       element.style.backgroundColor = divisionColor;
     });
   }
