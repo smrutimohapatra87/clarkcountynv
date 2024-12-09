2940c35fed5a02e05663999079a50ce669bac2f0
meejain <37831850+meejain@users.noreply.github.com>
2024-12-10 04:43:02 +0530

    Issue76 (#78)
    * adding more helix query
    
    * Adding the logic of recurring
    
    * Adding the logic of recurring
    
    * Fixing the lint issues
    
    ---------
    
    Co-authored-by: Meet Kumar Jain <meejain@meets-mbp-2.corp.adobe.com>
    Co-authored-by: Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
    Co-authored-by: Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>

--- templates/calendar/calendar.js
+++ templates/calendar/calendar.js
@@ -1,12 +1,22 @@
 import { div, section } from '../../scripts/dom-helpers.js';
 
-// const childCalendars = [
-//   'County Commissioners',
-//   'County Commission District A',
-//   'County Commission District B',
-//   'County Commissioners District C',
-//   'County Commissioners District D',
-// ];
+class Obj {
+  // eslint-disable-next-line max-len
+  constructor(title, start, end, allDay, daysOfWeek, startTime, endTime, startRecur, endRecur, url, backgroundColor, textColor) {
+    this.title = title;
+    this.start = start;
+    this.end = end;
+    this.allDay = allDay;
+    this.daysOfWeek = daysOfWeek;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.startRecur = startRecur;
+    this.endRecur = endRecur;
+    this.url = url;
+    this.backgroundColor = backgroundColor;
+    this.textColor = textColor;
+  }
+}
 
 // Fetching events from individual calendar sheets
 export async function fetchPlaceholders(prefix) {
@@ -39,28 +49,62 @@ export async function fetchPlaceholders(prefix) {
 }
 
 async function initializeCalendar() {
-  let eventsData = [];
+  let importedData = [];
+  const eventsList = [];
   const calendarEl = document.getElementById('calendar');
   // const data = getEventsManual();
   const normalizeCalendar = 'events';
   const placeholders = await fetchPlaceholders(normalizeCalendar);
-  eventsData = [...eventsData, ...placeholders.data];
+  importedData = [...importedData, ...placeholders.data];
   // eslint-disable-next-line no-undef
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
     headerToolbar: {
       left: 'prev,next today',
-      center: 'title',
+      center: 'addEventButton',
       right: 'dayGridMonth,timeGridWeek,timeGridDay',
     },
     navLinks: true, // can click day/week names to navigate views
     editable: true,
     selectable: true,
     dayMaxEvents: true,
-    events: eventsData,
+    // events: importedData,
     eventTimeFormat: { hour: 'numeric', minute: '2-digit' },
   });
   calendar.render();
+  importedData.forEach((event) => {
+    const startTime = event.startRecur.split('T')[1];
+    const endTime = event.endRecur.split('T')[1];
+    const url = window.location.origin + event.path;
+    const eventObj = new Obj(event.title, event.start, event.end, event.allDay, event.daysOfWeek, startTime, endTime, event.startRecur, event.endRecur, url, event['division-color'], event.textColor);
+    eventsList.push(eventObj);
+  });
+  eventsList.forEach((event) => {
+    if (event.daysOfWeek.length > 0) {
+      calendar.addEvent({
+        title: event.title,
+        allDay: false,
+        daysOfWeek: event.daysOfWeek,
+        startTime: event.startTime,
+        endTime: event.endTime,
+        startRecur: event.startRecur,
+        endRecur: event.endRecur,
+        url: event.url,
+        backgroundColor: event.backgroundColor,
+        textColor: event.textColor,
+      });
+    } else {
+      calendar.addEvent({
+        title: event.title,
+        start: event.start,
+        end: event.end,
+        allDay: event.allDay,
+        url: event.url,
+        backgroundColor: event.backgroundColor,
+        textColor: event.textColor,
+      });
+    }
+  });
 }
 
 export function loadfullcalendar() {
