8cf519761cae2a2df520217455daf7c8ca0ac504
Kent Livingston <klivings@Kents-MacBook-Pro.local>
2024-12-16 17:28:18 -0600

    push of css for text

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,6 @@ import {
   buildBlock,
   loadHeader,
   loadFooter,
-  decorateButtons,
   decorateIcons,
   decorateSections,
   decorateBlocks,
@@ -112,6 +111,47 @@ function buildAutoBlocks(main) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
   }
+}
+
+/**
+ * Decorates paragraphs containing a single link as buttons.
+ * @param {Element} element container element
+ */
+export function decorateButtons(element) {
+  element.querySelectorAll('a').forEach((a) => {
+    a.title = a.title || a.textContent;
+    if (a.href !== a.textContent) {
+      const up = a.parentElement;
+      const twoup = a.parentElement.parentElement;
+      if (!a.querySelector('img')) {
+        // TODO:  comment below will remove button links, should see if 
+        // we do css for no-button instead "Link (plain links, no buttons)"
+
+        // if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
+        //   a.className = 'button'; // default
+        //   up.classList.add('button-container');
+        // }
+        if (
+          up.childNodes.length === 1
+          && up.tagName === 'STRONG'
+          && twoup.childNodes.length === 1
+          && twoup.tagName === 'P'
+        ) {
+          a.className = 'button primary';
+          twoup.classList.add('button-container');
+        }
+        if (
+          up.childNodes.length === 1
+          && up.tagName === 'EM'
+          && twoup.childNodes.length === 1
+          && twoup.tagName === 'P'
+        ) {
+          a.className = 'button secondary';
+          twoup.classList.add('button-container');
+        }
+      }
+    }
+  });
 }
 
 /**
