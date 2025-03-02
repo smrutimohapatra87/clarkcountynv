b81fa057e6d4a5e5bcce82b512fe3fcd6c311daa
meejain <37831850+meejain@users.noreply.github.com>
2025-03-03 05:29:46 +0530

    Enable Highlight Column on home page - enabling YT videos with custom Image with Play Button + Carousal styling Improvisations (#454)
    * Fixing the column block
    
    * Fixing the column block
    
    * Fixing the column block
    
    * Fixing the column block
    
    * Fixing the column block
    
    * Fixing the column & carousal block
    
    * Fixing the column & carousal block
    
    * Fixing the column & carousal block
    
    ---------
    
    Co-authored-by: Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,26 +1,77 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import { a, i } from '../../scripts/dom-helpers.js';
+
+function enablePlaybutton(col, pic, redirectURL) {
+  const playButton = a({ class: 'explore-video-play' }, i({ class: 'play-button' }));
+  playButton.href = redirectURL;
+  col.append(pic);
+  col.append(playButton);
+  col.classList.add('explore-img');
+}
+
+/** allow for link attributes to be added by authors
+ * example usage = Text [class:button,target:_blank,title:Title goes here]
+ * @param main
+ */
+export function decorateLinks(element) {
+  element.querySelectorAll('a').forEach(($a) => {
+    // match text inside [] and split by '|'
+    const match = $a.textContent.match(/(.*)\[\[([^\]\]]*)]/);
+    if (match) {
+      // eslint-disable-next-line no-unused-vars
+      const [_, linkText, attrs] = match;
+      $a.textContent = linkText.trim();
+      $a.setAttribute('title', $a.textContent);
+      attrs.split(',').forEach((attr) => {
+        let [key, ...value] = attr.trim().split(':');
+        key = key.trim().toLowerCase();
+        value = value.join().trim();
+        if (key) $a.setAttribute(key, value);
+      });
+    }
+  });
+}
 
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    [...row.children].forEach((col, i) => {
-      if (i === 0 && col.querySelector('a')) {
-        const imgSrc = col.querySelector('a').href;
+    [...row.children].forEach((col) => {
+      if (col.querySelectorAll('a') && col.querySelectorAll('p:not(:has(a)), h1, h2, h3, h4, h5, h6, div').length === 0) {
+        const imgSrc = col.querySelectorAll('a')[0].href;
         const pic = createOptimizedPicture(imgSrc, imgSrc.split('/').pop());
-        if (pic) {
-          col.replaceWith(pic);
+        if (col.querySelectorAll('a')[1]) {
+          const redirectElement = col.querySelectorAll('a')[1];
+          if (pic) {
+            col.innerHTML = '';
+            if (redirectElement.href.includes('youtube.com') || redirectElement.href.includes('youtu.be')) {
+              enablePlaybutton(col, pic, redirectElement.href);
+            } else {
+              const redirect = a({ class: 'redirect' });
+              redirect.href = redirectElement.href;
+              redirect.append(pic);
+              col.append(pic);
+              col.classList.add('explore-img');
+            }
+          }
+        } else if (pic) {
+          col.innerHTML = '';
+          col.append(pic);
+          col.classList.add('explore-img');
         }
+      } else {
+        col.classList.add('explore-item');
+        decorateLinks(col);
       }
     });
   });
   const cols = [...block.firstElementChild.children];
 
   block.classList.add(`columns-${cols.length}-cols`);
-  for (let i = 0; i < cols.length; i += 1) {
-    cols[i].classList.add(`column${i + 1}`);
-    [...cols[i].children].forEach((child) => {
+  for (let counter = 0; counter < cols.length; counter += 1) {
+    cols[counter].classList.add(`column${counter + 1}`);
+    [...cols[counter].children].forEach((child) => {
       child.classList.remove('button-container');
       child.classList.add('columns-paragraph');
-      child.classList.add(`column${i + 1}-paragraph`);
+      child.classList.add(`column${counter + 1}-paragraph`);
     });
   }
 
