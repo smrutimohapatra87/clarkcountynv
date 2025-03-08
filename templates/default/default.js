c893f19c99a28a1adf4408ee7e1426630b7a1840
meejain <37831850+meejain@users.noreply.github.com>
2025-03-08 22:42:27 +0530

    Fixing the console error messages (#602)
    Co-authored-by: Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>

--- templates/default/default.js
+++ templates/default/default.js
@@ -30,21 +30,24 @@ export function decorateLinks(element) {
 export default async function decorate(doc) {
   const $main = doc.querySelector('main');
   const $leftsection = document.querySelector('.leftsection');
-  const $clickElement = $leftsection.querySelector('.default-content-wrapper > p');
-  const $activeElement = $leftsection.querySelector('.accordion-ml.block');
+  if ($leftsection) {
+    const $clickElement = $leftsection.querySelector('.default-content-wrapper > p');
+    const $activeElement = $leftsection.querySelector('.accordion-ml.block');
 
-  $clickElement.addEventListener('click', () => {
-    $clickElement.classList.toggle('active');
-    $activeElement.classList.toggle('active');
-    const height = document.querySelector(':root');
-    const originalHeight = height.style.getPropertyValue('--original-height');
-    height.style.setProperty('--height', `${originalHeight}`);
-    if (!$clickElement.classList.contains('active')) {
-      $activeElement.querySelectorAll('details[open]').forEach((detail) => {
-        detail.removeAttribute('open');
-      });
-    }
-  });
+    $clickElement.addEventListener('click', () => {
+      $clickElement.classList.toggle('active');
+      $activeElement.classList.toggle('active');
+      const height = document.querySelector(':root');
+      const originalHeight = height.style.getPropertyValue('--original-height');
+      height.style.setProperty('--height', `${originalHeight}`);
+      if (!$clickElement.classList.contains('active')) {
+        $activeElement.querySelectorAll('details[open]').forEach((detail) => {
+          detail.removeAttribute('open');
+        });
+      }
+    });
+
+  }
 
   const $rightsection = document.querySelector('.rightsection');
 
