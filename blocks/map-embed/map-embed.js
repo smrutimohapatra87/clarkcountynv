3c791f4716699fb0dd218cf849f4df93f5d6b38a
Akash Jain <57232121+akasjain2@users.noreply.github.com>
2025-03-19 20:10:12 +0530

    Add table style, text color, title to map-embed (#953)
    * Add table style
    
    * Add title to map-embed
    
    * Fix linting
    
    * Add color and ul style

--- blocks/map-embed/map-embed.js
+++ blocks/map-embed/map-embed.js
@@ -1,20 +1,22 @@
-import { iframe } from '../../scripts/dom-helpers.js';
-
-let $mapEmbedUrl = '';
+import { iframe, p } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    if (row.querySelector('a')) {
-      $mapEmbedUrl = row.querySelector('a').href;
+    const mapEmbedEl = row.querySelector('a');
+    if (mapEmbedEl) {
+      const mapLink = mapEmbedEl.href;
       const $iframe = iframe(
         {
-          src: $mapEmbedUrl,
+          src: mapLink,
           allowFullscreen: true,
           frameBorder: 0,
           class: 'map-embed',
         },
       );
       row.replaceWith($iframe);
+      if (mapEmbedEl.textContent.trim() !== mapLink) {
+        $iframe.parentElement.appendChild((p({ class: 'map-title' }, mapEmbedEl.textContent.trim())));
+      }
     }
   });
 }
