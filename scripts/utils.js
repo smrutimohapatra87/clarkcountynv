71b7409af4ea7249ea73f2f95b5b4d812a687458
meejain <37831850+meejain@users.noreply.github.com>
2025-05-22 03:50:48 +0530

    Fixing the links (#1336)
    Co-authored-by: Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>

--- scripts/utils.js
+++ scripts/utils.js
@@ -34,12 +34,7 @@ export function normalizeString(str) {
 export function externalLinks(main) {
   const links = main.querySelectorAll('a[href]');
   links.forEach((linkItem) => {
-    const hrefURL = new URL(linkItem.href);
-    console.log(hrefURL.hostname);
-    console.log(window.location.hostname);
-    if (hrefURL.pathname.includes('pdf') || hrefURL.hostname !== window.location.hostname) {
-      linkItem.setAttribute('target', '_blank');
-    }
+    linkItem.setAttribute('target', '_blank');
   });
 }
 
